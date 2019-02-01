30e8f70ddaa11295af5116c88295d68948d2e993
ndallen <ecommerceevolution@gmail.com>
2019-02-01 14:59:19 +0700

    created javascript function to amend DOM with ol and li element

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log()
+
+const songs = [
+  "Have A Cigar.mp3",
+  "Shine On You Crazy Diamond (Part I-V).mp3 ",
+  "Shine On You Crazy Diamond (Part VI-IX).mp3" ,
+  "Welcome To The Machine.mp3" ,
+  "Wish You Were Here.mp3" 
+];
+
+const createSongList = () => {
+  const list = document.createElement('ol');
+
+  for(let i = 0; i < songs.length; i++) {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode(songs[i]));
+    list.appendChild(item);
+  }
+  return list;
+}
+
+document.getElementById('songList').appendChild(createSongList());
